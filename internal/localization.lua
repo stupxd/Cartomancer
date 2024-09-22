642f6d14bc8760d3e1216b4f7294ae44161fa40f
stupxd <uknownvector@gmail.com>
2024-09-22 03:00:18 +0300

    fix loc file name

--- internal/localization.lua
+++ internal/localization.lua
@@ -1,7 +1,7 @@
 if Cartomancer.use_smods() then return end
 
 -- Vanilla will only support default loc cuz yes.
-local loc_table = Cartomancer.load_mod_file('localization/default.lua', 'localization')
+local loc_table = Cartomancer.load_mod_file('localization/en-us.lua', 'localization')
 
 -- Credits: Steamodded
 -- I was lazy and it's not like I'm going to code anything different from this anyways~
