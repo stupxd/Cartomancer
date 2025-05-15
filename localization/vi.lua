85969e850362b78cf7d59fad35bbe777ffb81766
stupxd <uknownvector@gmail.com>
2025-05-15 20:27:54 +0300

    Add Vietnamese localization

---
+++ localization/vi.lua
@@ -0,0 +1,70 @@
+return {
+    misc = {
+        dictionary = {
+            carto_settings_compact_deck = "Ép gọn bộ bài",
+            carto_settings_deck_view = "Xem bộ bài",
+            carto_settings_jokers = "Joker",
+            carto_settings_flames = "Lửa",
+            carto_settings_other = "Khác",
+            carto_settings_keybinds = "Phím tắt",
+
+            carto_compact_deck_enabled = "Giới hạn lá bài trong bộ bài",
+            carto_compact_deck_visible_cards = "Giới hạn lá bài ",
+
+            carto_deck_view_stack_enabled = "Chồng lá bài trong phần xem bộ bài",
+            carto_deck_view_hide_drawn_cards = "Ẩn lá đã bốc",
+            carto_deck_view_stack_modifiers = "Chồng mọi sửa đổi",
+            carto_deck_view_stack_chips = "Chồng giá trị chip khác nhau",
+            -- carto_deck_view_stack_suits = "Stack all suits", -- Do not think this is necessary if steamodded adds suit pages.
+            carto_deck_view_stack_x_color = "Màu ",
+            carto_deck_view_stack_background_opacity = "Độ mờ ",
+            carto_deck_view_stack_pos = "Vị trí ",
+            carto_deck_view_stack_pos_vertical_options = {
+                "đỉnh",
+                "giữa",
+                "đáy"
+            },
+            carto_deck_view_stack_pos_horizontal_options = {
+                "trái",
+                "giữa",
+                "phải"
+            },
+            carto_deck_view_unique_cards = "Lá riêng biệt:",
+
+            carto_improved_hand_sorting = "Nhiều lựa chọn sắp xếp bài hơn",
+            carto_dynamic_hand_align = "Xếp bài đẹp hơn với số lá trong tay lớn",
+            carto_hide_tags = "Ẩn nhãn bỏ qua",
+            carto_hide_consumables = "Ẩn lá tiêu thụ",
+            carto_hide_deck = "Ẩn bộ bài",
+
+            carto_flames_chips = "Chip",
+            carto_flames_mult = "Nhân",
+            carto_flames_intensity_min = "Cường độ tối thiểu ",
+            carto_flames_intensity_max = "Cường độ tối đa ",
+            carto_flames_relative_intensity = "Cường độ tỉ lệ thuận với blind hiện tại",
+            carto_flames_intensity_vanilla = "Bỏ qua cường độ tối thiểu/tối đa",
+            carto_flames_gasoline_title = "Xem trước",
+            carto_flames_gasoline = "",
+            carto_flames_volume = "Thể tích lửa ",
+            
+            carto_jokers_controls_buttons = "Hiện nút chỗ joker",
+            carto_jokers_controls_show_after = "sau khi số lượng joker vượt ",
+            --carto_jokers_hide_keybind = "Hide hovered joker with Alt + 1",
+            carto_jokers_hide = "Ẩn",
+            carto_jokers_show = "Hiện",
+            carto_jokers_zoom = "Phóng to",
+
+            carto_waiting_keybind = "Đang chờ nhập liệu...",
+            carto_kb_hide_joker =  "Ẩn joker",
+            carto_kb_toggle_tags = "Ẩn/hiện nhãn bỏ qua",
+            carto_kb_toggle_consumables = "Ẩn/hiện lá tiêu thụ",
+            carto_kb_toggle_jokers = "Ẩn/hiện joker",
+            carto_kb_toggle_jokers_buttons = "Ẩn/hiện nút joker",
+
+            carto_blinds_info_setting = "Hiện tab thông tin Blind",
+            carto_blinds_info_current = "Hiện tại",
+            carto_blinds_info_extra = "Lịch sử",
+            carto_blinds_info_title = "Lịch sử Boss Blind",
+        }
+    }
+}
