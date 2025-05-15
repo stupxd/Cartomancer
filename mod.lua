4c8590150344acf20d79ce64b3994967dd202175
stupxd <uknownvector@gmail.com>
2025-05-15 17:18:20 +0300

    bump version

--- mod.lua
+++ mod.lua
@@ -6,7 +6,7 @@
 --- PRIORITY: 69
 --- BADGE_COLOR: FFD700
 --- DISPLAY_NAME: Cartomancer
---- VERSION: 4.14
+--- VERSION: 4.15
 
 ----------------------------------------------
 ------------MOD CODE -------------------------
