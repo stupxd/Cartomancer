f0cbbd901673a9ed5292e8a19cff1ac0eade4795
Talkie <wtalkie104@gmail.com>
2025-11-14 12:09:56 +0900

    Create Ja.lua
    Add Japanese localization.

---
+++ localization/ja.lua
@@ -0,0 +1,72 @@
+-- localized by @koukichi_kkc 
+-- 気になる点がありましたらお気軽にDMまでご相談ください!
+return {
+    misc = {
+        dictionary = {
+            carto_settings_compact_deck = "山札表示",
+            carto_settings_deck_view = "フルデッキ表示",
+            carto_settings_jokers = "ジョーカー",
+            carto_settings_flames = "炎の演出",
+            carto_settings_other = "その他",
+            carto_settings_keybinds = "キー設定",
+
+            carto_compact_deck_enabled = "山札の表示枚数を制限する",
+            carto_compact_deck_visible_cards = "枚数上限 ",
+
+            carto_deck_view_stack_enabled = "同一カードをまとめて表示する",
+            carto_deck_view_hide_drawn_cards = "既に引いたカードを非表示にする",
+            carto_deck_view_stack_modifiers = "エディションの違いを無視する",
+            carto_deck_view_stack_chips = "追加チップの違いを無視する",
+            -- carto_deck_view_stack_suits = "Stack all suits", -- Do not think this is necessary if steamodded adds suit pages.
+            carto_deck_view_stack_x_color = "色 ",
+            carto_deck_view_stack_background_opacity = "不透明度 ",
+            carto_deck_view_stack_pos = "表示位置 ",
+            carto_deck_view_stack_pos_vertical_options = {
+                "上",
+                "中央",
+                "下"
+            },
+            carto_deck_view_stack_pos_horizontal_options = {
+                "左",
+                "中央",
+                "右"
+            },
+            carto_deck_view_unique_cards = "固有のカード枚数 ",
+
+            carto_improved_hand_sorting = "手札のソートオプションを増やす(ソートボタン2度押しで逆順)",
+            carto_dynamic_hand_align = "手札サイズが非常に大きい場合のアニメーションを最適化",
+            carto_hide_tags = "タグを非表示",
+            carto_hide_consumables = "消耗品を非表示",
+            carto_hide_deck = "非表示",
+
+            carto_flames_chips = "Chips",
+            carto_flames_mult = "Mult",
+            carto_flames_intensity_min = "最小サイズ ",
+            carto_flames_intensity_max = "最大サイズ ",
+            carto_flames_relative_intensity = "ノルマスコアの超過量に応じて大きさを変える",
+            carto_flames_intensity_vanilla = "最小/最大設定を無視する",
+            carto_flames_gasoline_title = "プレビュー",
+            carto_flames_gasoline = "プレビュー",
+            carto_flames_volume = "効果音の音量",
+            
+            carto_jokers_controls_buttons = "所持ジョーカー枚数が多い場合にズーム機能を使用する",
+            carto_jokers_controls_show_after = "ズーム機能が有効になる枚数",
+            --carto_jokers_hide_keybind = "Hide hovered joker with Alt + 1",
+            carto_jokers_hide = "非表示",
+            carto_jokers_show = "表示",
+            carto_jokers_zoom = "ズーム",
+
+            carto_waiting_keybind = "設定するキーを押してください",
+            carto_kb_hide_joker =  "個別にジョーカーを非表示",
+            carto_kb_toggle_tags = "タグの表示/非表示",
+            carto_kb_toggle_consumables = "消耗品の表示/非表示",
+            carto_kb_toggle_jokers = "ジョーカーの表示/非表示",
+            carto_kb_toggle_jokers_buttons = "ズーム機能の有効/無効",
+
+            carto_blinds_info_setting = "ボスブラインドの履歴を表示する",
+            carto_blinds_info_current = "現在",
+            carto_blinds_info_extra = "履歴",
+            carto_blinds_info_title = "ボスブラインド履歴",
+        }
+    }
+}
